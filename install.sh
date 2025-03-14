400716dd417a8595ff71333f3bb7dd490c82b2f8
Serafadam <serafadam@gmail.com>
2025-03-14 19:00:54 +0100

    renaming, updates for tmux

--- install.sh
+++ install.sh
@@ -29,7 +29,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 pip3 install cppcheck
 
-git clone --branch release-0.10 https://github.com/neovim/neovim.git && cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo && make install
+cd  /usr/local/bin/ && \
+wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz && \
+tar zxfv nvim-linux-x86_64.tar.gz && \
+rm -rf nvim-linux-x86_64.tar.gz && \
+ln -sf /usr/local/bin/nvim-linux-x86_64/bin/nvim /usr/local/bin/
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
